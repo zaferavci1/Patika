f33ec418fda65c83bae4945ef6cf5b8bf89724f7
zaferavci1 <avcizafer963@gmail.com>
2022-04-22 04:58:16 +0300

    https://app.patika.dev/zaferavci

---
+++ Java101/src/SayıSıralama.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SayıSıralama {
+    public static void main(String[] args) {
+
+        Scanner abc = new Scanner(System.in);
+        int x, y, z;
+
+        System.out.println("Sayi 1'i giriniz");
+        x = abc.nextInt();
+
+        System.out.println("Sayi 2'yi giriniz");
+        y = abc.nextInt();
+
+        System.out.println("Sayi 3'u giriniz");
+        z = abc.nextInt();
+
+        if (x > y && x > z) {
+            System.out.println(x);
+        } else if (y > x && y > z) {
+            System.out.println(y);
+        } else if (z>x&&z>y){
+            System.out.println(z);
+        }
+
+    }
+}
