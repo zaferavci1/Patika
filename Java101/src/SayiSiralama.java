9a68b0c1395b1027610ca80a852d7eb25ef90d69
zaferavci1 <avcizafer963@gmail.com>
2022-04-22 22:43:07 +0300

    https://app.patika.dev/zaferavci

--- Java101/src/SayiSiralama.java
+++ Java101/src/SayiSiralama.java
@@ -16,11 +16,39 @@ public class SayiSiralama {
         z = abc.nextInt();
 
         if (x > y && x > z) {
-            System.out.println(x);
-        } else if (y > x && y > z) {
-            System.out.println(y);
-        } else if (z>x&&z>y){
-            System.out.println(z);
+            System.out.println("En buyuk sayi: "+x);
+            if (y>z){
+                System.out.println("Ortadaki sayi: "+y);
+                System.out.println("En kucuk sayi: "+z);
+            }
+            else if (z>y){
+                System.out.println("Ortadaki sayi: "+z);
+                System.out.println("En kucuk sayi: "+y);
+            }
+
+        }
+        else if (y > x && y > z) {
+            System.out.println("En buyuk sayi: "+y);
+            if (x>z)
+            {
+                System.out.println("Ortadaki sayi: "+x);
+                System.out.println("En kucuk sayi: "+z);
+            }
+            else if (z>x){
+                System.out.println("Ortadaki sayi: "+z);
+                System.out.println("En kucuk sayi: "+x);
+            }
+        }
+        else if (z>x&&z>y){
+            System.out.println("En buyuk sayi: "+z);
+            if (x>y){
+                System.out.println("Ortadaki sayi: "+x);
+                System.out.println("En kucuk sayi: "+y);
+            }
+            else if (y>x){
+                System.out.println("Ortadaki sayi: "+y);
+                System.out.println("En kucuk sayi: "+x);
+            }
         }
 
     }
