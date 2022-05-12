43bf6f998ec000ebfb47caa8147ee78496f17733
zaferavci1 <avcizafer963@gmail.com>
2022-05-12 12:58:14 +0300

    app.patika.dev/zaferavci

--- Java101/Diziler/src/DizidekiElemanlarinOrtalamasi.java
+++ Java101/Diziler/src/DizidekiElemanlarinOrtalamasi.java
@@ -2,12 +2,12 @@ import java.sql.Array;
 
 public class DizidekiElemanlarinOrtalamasi {
     public static void main(String[] args) {
-        int[] dizi={8,10,15,16,23,24};
-        int value=0;
-        for (int i=0;i< dizi.length;i++){
-            value+=dizi[i];
+        int[] dizi = {8, 10, 15, 16, 23, 24};
+        int value = 0;
+        for (int i = 0; i < dizi.length; i++) {
+            value += dizi[i];
         }
-        int ortalama=value/(dizi.length);
-        System.out.println("Dizinin Ortalamasi: "+ortalama);
+        int ortalama = value / (dizi.length);
+        System.out.println("Dizinin Ortalamasi: " + ortalama);
     }
 }
---
+++ Java101/Diziler/src/EnYakinMinMax.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EnYakinMinMax {
+    public static void main(String[] args) {
+        int[] dizi = {12, 23, 56, 89, 45, 78, 24, 59};
+
+        int a = 0, b = 0, maxSayi = 0;
+        int[] min = new int[dizi.length];
+        int[] max = new int[dizi.length];
+
+        Scanner abc = new Scanner(System.in);
+        System.out.print("Sayi giriniz: ");
+        int value = abc.nextInt();
+
+        for (int i = 0; i < dizi.length; i++) {
+            if (dizi[i] < value) {
+
+                min[a] = dizi[i];
+                a++;
+            } else if (dizi[i] > value) {
+                max[b] = dizi[i];
+                b++;
+            }
+        }
+
+        System.out.println("Dizi: "+Arrays.toString(dizi));
+        System.out.println("Girilen sayi: "+value);
+        Arrays.sort(min);
+        System.out.println("Girilen sayidan kucuk en yakin sayi : " + min[(min.length) - 1]);
+
+        Arrays.sort(max);
+        for (int i = 0; i < max.length; i++) {
+            if (max[i] > 0) {
+                maxSayi = max[i];
+                break;
+            }
+        }
+        System.out.print("Girilen sayidan buyuk en yakin sayi : " + maxSayi);
+
+
+    }
+}
+
+
