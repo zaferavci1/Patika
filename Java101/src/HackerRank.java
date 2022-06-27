1f1acb9f04f4479f128e22007578a60e610c6590
zaferavci1 <avcizafer963@gmail.com>
2022-06-27 16:01:19 +0300

    Dosya Konumları Güncellendi

--- Java101/src/HackerRank.java
+++ Java101/src/HackerRank.java
@@ -1,20 +1,27 @@
+import java.util.Locale;
 import java.util.Scanner;
 
-public class HackerRank {
-
-    public static void Patika(int num){
-        num--;
-        if (num == 0)
-            return;
-        System.out.print(num + ",");
-        Patika(num);
-    }
+public class HackerRank{
 
 
     public static void main(String[] args) {
 
+        int x=0;
+        Scanner abc=new Scanner(System.in);
+        String s=abc.nextLine();
+        Scanner ab=new Scanner(s);
+        while(ab.hasNext()){
+            x++;
+            if (x==4)
+                break;
+        }
+            System.out.println(x);
 
     }
 
-}
 
+
+
+
+
+}
