0d38b98e219a78704df955da26218a82566aff05
zaferavci1 <avcizafer963@gmail.com>
2022-06-30 18:36:38 +0300

    c#101 try catch

---
+++ C#101/Pratikler/hataYonetimi/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace hataYonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try
+            // {
+            //     Console.WriteLine("Bir sayi giriniz");
+            //     int sayi = Convert.ToInt32(Console.ReadLine());
+            //     Console.WriteLine("Girmiş olduğunuz sayi: " + sayi);
+            // }
+            // catch (Exception ex)
+            // {
+            //     Console.WriteLine("Hata: " + ex.Message.ToString());
+            // }
+            //finally
+            //{
+            //    Console.WriteLine("İşlem Tamamlandı.");
+            //}
+
+            try
+            {
+                //int a = int.Parse(null);
+               // int a=int.Parse("test");
+                int b=int.Parse("-2000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("VEri tipi uygun değil.");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                System.Console.WriteLine("Çok küçük yada çok büyük değer girdiniz.");
+                System.Console.WriteLine(ex);
+            }
+            finally{
+                System.Console.WriteLine("İşlem başarıyla tamamalandı");
+            }
+        }
+    }
+}
