dc8cb8627459e2eb3c363291aefa06f7eacf9d05
zaferavci1 <avcizafer963@gmail.com>
2022-07-05 18:53:04 +0300

    c#101 metot overloading

---
+++ C#101/Pratikler/metotOverloading/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace switchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+            string sayi="9999";
+            bool sonuc=int.TryParse(sayi,out int outSayi);
+            if (sonuc)
+            {
+                System.Console.WriteLine("Başarılı Çevirme");
+                System.Console.WriteLine(outSayi);
+            }
+            else
+                System.Console.WriteLine("Başarısız");
+
+            Metotlar ornek=new Metotlar();
+            ornek.Topla(2,3,out int toplamSonuc); 
+            System.Console.WriteLine(toplamSonuc);
+
+            //metot aşırı yükleme - overloading
+            int ifade=999;
+            ornek.ekranaYazdir(Convert.ToString(ifade));
+        }
+    }
+    class Metotlar
+    {
+        public void Topla(int a,int b,out int toplam){
+            toplam=a+b;
+        }
+        public void ekranaYazdir(string veri){
+            System.Console.WriteLine("string metot"+veri);
+        }
+        public void ekranaYazdir(int sayi){
+            System.Console.WriteLine("int metot"+sayi);
+        }
+
+    }
+}
